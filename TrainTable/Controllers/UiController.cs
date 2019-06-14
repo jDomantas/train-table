4c6ac5c1b57cde34c97df6cea65108b0fecde2b0
Domantas Jadenkus <djadenkus@gmail.com>
2019-06-14 04:09:03 +0300

    such ui very wow

---
+++ TrainTable/Controllers/UiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainTable.Controllers
+{
+    [Route("/")]
+    public class UiController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return File("index.html", "text/html");
+        }
+    }
+}
--- TrainTable/Startup.cs
+++ TrainTable/Startup.cs
@@ -67,6 +67,7 @@ namespace TrainTable
             }
 
             app.UseMvc();
+            app.UseStaticFiles();
         }
     }
 }
