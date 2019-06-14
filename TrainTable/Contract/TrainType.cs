9fb7141ddd881ae2cea56e65da90c785f096614b
Vytautas Strimaitis <vstrimaitis@gmail.com>
2019-06-14 03:38:45 +0300

    Return TrainType as string

--- TrainTable/Contract/TrainType.cs
+++ TrainTable/Contract/TrainType.cs
@@ -1,5 +1,9 @@
-﻿namespace TrainTable.Contract
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace TrainTable.Contract
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum TrainType
     {
         Electric,
